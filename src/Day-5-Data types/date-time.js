e3f4a493ee96d578a8842c0cab83b6d7f2bb0067
gunjan <2304030100555@silveroakuni.ac.in>
2025-12-03 21:39:04 +0530

    updated strycture.js with code

---
+++ src/Day-5-Data types/date-time.js
@@ -0,0 +1,12 @@
+// date-time.js
+"use strict";
+
+let now = new Date();
+console.log("Current Date and Time:", now);
+
+console.log("Year:", now.getFullYear());
+console.log("Month:", now.getMonth() + 1); // Month 0-11
+console.log("Day:", now.getDate());
+console.log("Hours:", now.getHours());
+console.log("Minutes:", now.getMinutes());
+console.log("Seconds:", now.getSeconds());
